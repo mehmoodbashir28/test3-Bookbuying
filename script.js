51dc471c55ea5d1c587e2c5fd55486c8d3bc04c1
Mehmood Bashir <39158675+mehmoodbashir28@users.noreply.github.com>
2018-10-13 17:32:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let i = 0;
+let price = 5.99; 
+function functionPlus() {
+    document.getElementById("numberOfBooks").innerHTML = ++i;
+    document.getElementById("result").innerHTML = price * i;
+}
+function functionMinus() {
+    if (i > 0)
+    i =
+    document.getElementById("numberOfBooks").innerHTML = --i;
+    document.getElementById("result").innerHTML = price * i;
+}
+
+
+
